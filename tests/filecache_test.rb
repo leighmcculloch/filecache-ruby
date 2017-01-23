c7def2d5c668dcaac12939e303f6e6da8ae2fc11
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-01-23 20:08:53 +0000

    Fix whitespace issues

--- tests/filecache_test.rb
+++ tests/filecache_test.rb
@@ -6,16 +6,16 @@ class FileCacheTest < Test::Unit::TestCase
   KEY2 = "key2"
   VALUE1 = "value1"
   VALUE2 = "value2"
-  
+
   def test_basic
     f = FileCache.new("unit-test", "/tmp")
-    
+
     f.set(KEY1, VALUE1)
     assert_equal(f.get(KEY1), VALUE1, "set/get")
-    
+
     f.delete(KEY1)
     assert_nil(f.get(KEY1), "delete")
-    
+
     assert_equal(
       f.get_or_set(KEY1) do
         VALUE1
@@ -23,7 +23,7 @@ class FileCacheTest < Test::Unit::TestCase
       VALUE1,
       "set_or_get"
     )
-    
+
     assert_equal(
       f.get_or_set(KEY1) do
         VALUE2
@@ -37,13 +37,13 @@ class FileCacheTest < Test::Unit::TestCase
 
     f.set(KEY1, VALUE1)
     assert_equal(f.get(KEY1), VALUE1, "set on previously deleted key")
-    
+
     f.set(KEY1, VALUE2)
     assert_equal(f.get(KEY1), VALUE2, "set new value on previously set key")
-    
+
     f.purge
     assert_equal(f.get(KEY1), VALUE2, "purge has no effect on cache with expiry=0")
-    
+
     f.clear
     assert_nil(f.get(KEY1), "clear removes previously set value")
   end
